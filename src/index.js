3a319b36702125adeff694a6123f33c60cfb63b2
Myles Lewando <myles.lewando@opendataservices.coop>
2024-07-08 07:54:24 +0100

    Restructure code & add better comments

--- src/index.js
+++ src/index.js
@@ -1,43 +1,39 @@
+// Import libraries
 import * as d3 from 'd3';
 import dagreD3 from 'dagre-d3';
 
-// Create graph
-const g = new dagreD3.graphlib.Graph();
-g.setGraph({});
-
-// Create nodes
-g.setNode(0, { label: 'A' });
-g.setNode(1, { label: 'B' });
-
-// Create edges
-g.setEdge(0, 1, { label: 'Test' });
-
-// Create renderer
-const render = new dagreD3.render();
-
-// Target svg and create group
-const svg = d3.select('svg').attr('width', window.innerWidth).attr('height', window.innerHeight);
+// Set up svg with D3
+const svg = d3.select('#graph').attr('width', window.innerWidth).attr('height', window.innerHeight);
 const inner = svg.append('g');
 
-// Set up zoom support
+// Add zoom support
 const zoom = d3.zoom().on('zoom', (element) => {
   inner.attr('transform', element.transform);
 });
 svg.call(zoom);
 
-// Run renderer
-render(inner, g);
+// Set up graph
+const graph = new dagreD3.graphlib.Graph().setGraph({ rankDir: 'LR' });
 
+// Draw nodes and edges
+graph.setNode(0, { label: 'A' });
+graph.setNode(1, { label: 'B' });
+graph.setEdge(0, 1, { label: 'to' });
+
+// Render graph
+const render = new dagreD3.render();
+render(inner, graph);
+
+// Get width and height of graph and svg element
 const bounds = inner.node().getBBox();
 const width = svg.attr('width');
 const height = svg.attr('height');
+
+// Centre and scale graph to fit page
 svg.call(
   zoom.transform,
   d3.zoomIdentity
-    // Centre graph
     .translate(width / 2, height / 2)
-    // Scale graph to svg element
     .scale(Math.min(width / bounds.width, height / bounds.height))
-    // Fit graph to svg element
     .translate(-bounds.x - bounds.width / 2, -bounds.y - bounds.height / 2)
 );
